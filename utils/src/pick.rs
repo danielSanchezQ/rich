b27e5d2599aa46b8cf8bd47682c7cc12fe5e4a2e
danielsanchezq <daniel.sanchez@iohk.io>
2021-01-07 15:45:20 +0100

    Added pick_bool test Fixed misplaced Item reference

--- utils/src/pick.rs
+++ utils/src/pick.rs
@@ -1,7 +1,7 @@
 /// Pick the first non-none bool or return the last value
 pub fn pick_bool<'a, Values>(values: Values) -> bool
 where
-    Values: IntoIterator<Item = Option<&'a bool>>,
+    Values: IntoIterator<Item = &'a Option<bool>>,
 {
     // original algorithm asserts lenght: https://github.com/danielSanchezQ/rich-1/blob/master/rich/_pick.py
     // but we will denote an empty one as false
@@ -13,4 +13,15 @@ where
         }
     }
     false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::pick_bool;
+    #[test]
+    fn test_pick_bool() {
+        assert_eq!(pick_bool(&[]), false);
+        assert_eq!(pick_bool(&[Some(true)]), true);
+        assert_eq!(pick_bool(&[Some(false), Some(false)]), false);
+    }
 }
