383b3e453b19482b93688a3b5541d1839d92ba4b
danielsanchezq <daniel.sanchez@iohk.io>
2021-01-09 15:45:38 +0100

    Implemented segment split lines, adjust line length and fixed apply style return type to references

--- segment/src/lib.rs
+++ segment/src/lib.rs
@@ -1,8 +1,8 @@
 use cells::{cell_len, set_cell_size, DEFAULT_CELL_LEN_CACHE};
-use style::Style;
+use style::{Style, StyleBuilder};
 
 /// A piece of text with associated style
-#[derive(Clone)]
+#[derive(Clone, Eq, PartialEq, Debug)]
 pub struct Segment {
     /// Raw text
     text: String,
@@ -63,14 +63,14 @@ impl Segment {
     }
 
     /// Apply a style to an iterable of segments
-    pub fn apply_style<Segments>(
+    pub fn apply_style<'a, Segments>(
         segments: Segments,
         style: Option<Style>,
-    ) -> impl Iterator<Item = Segment>
+    ) -> impl Iterator<Item = Segment> + 'a
     where
-        Segments: IntoIterator<Item = Segment>,
+        Segments: IntoIterator<Item = &'a Segment> + 'a,
     {
-        segments.into_iter().map(move |s: Segment| {
+        segments.into_iter().map(move |s: &Segment| {
             if let Some(style) = &style {
                 Self::new(
                     &s.text,
@@ -82,7 +82,7 @@ impl Segment {
                     s.is_control,
                 )
             } else {
-                s
+                s.clone()
             }
         })
     }
@@ -100,11 +100,144 @@ impl Segment {
             .filter(move |s| s.is_control == is_control)
     }
 
+    /// Adjust a line to a given width (cropping or padding as required)
+    pub fn adjust_line_length(
+        line: &[Segment],
+        length: usize,
+        style: Option<Style>,
+        padding: Option<bool>,
+    ) -> Vec<Segment> {
+        let padding = padding.unwrap_or(true);
+        let line_length: usize = line.iter().map(|s| s.cell_len()).sum();
+        if line_length < length {
+            if padding {
+                line.iter()
+                    .chain(
+                        [Segment::new(
+                            &" ".repeat(length - line_length),
+                            style,
+                            false,
+                        )]
+                        .iter(),
+                    )
+                    .cloned()
+                    .collect()
+            } else {
+                line.iter().cloned().collect()
+            }
+        } else if line_length > length {
+            let mut new_line: Vec<Segment> = Vec::new();
+            let mut line_length = 0;
+            for segment in line {
+                let segment_length = segment.cell_len();
+                if (line_length + segment_length) < length || segment.is_control {
+                    new_line.push(segment.clone());
+                    line_length += segment_length;
+                } else {
+                    let (text, segment_style, _) = segment.as_tuple();
+                    let text = set_cell_size(text, length - line_length);
+                    new_line.push(Segment::new(&text, segment_style.clone(), false));
+                }
+            }
+            new_line
+        } else {
+            line.iter().cloned().collect()
+        }
+    }
+
+    // fn split_and_crop(&self) -> Vec<Self> {
+    //     if self.text.contains('\n') && !self.is_control {
+    //         let (text, style, _) = self.as_tuple();
+    //         while !text.is_empty() {}
+    //     } else {
+    //         vec![self.clone()]
+    //     }
+    // }
+
     /// Split a sequence of segments in to a list of lines
-    pub fn split_lines<Segments>(segments: Segments) -> impl Iterator<Item = Segment>
+    pub fn split_lines<'a, Segments>(segments: Segments) -> Vec<Vec<Segment>>
     where
-        Segments: IntoIterator<Item = Segment>,
+        Segments: IntoIterator<Item = &'a Segment>,
     {
-        segments.into_iter()
+        let mut res: Vec<Vec<Segment>> = Vec::new();
+        let mut line: Vec<Segment> = Vec::new();
+        for segment in segments {
+            if segment.text.contains('\n') && !segment.is_control {
+                let (mut text, style, _) = segment.as_tuple();
+                while !text.is_empty() {
+                    match text.splitn(2, '\n').collect::<Vec<&str>>().as_slice() {
+                        [_text, next] => {
+                            line.push(Segment::new(_text, style.clone(), false));
+                            res.push(line);
+                            line = Vec::new();
+                            text = next;
+                        }
+                        [_text] => {
+                            text = "";
+                            line.push(Segment::new(_text, style.clone(), false));
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+            } else {
+                line.push(segment.clone());
+            }
+        }
+        if !line.is_empty() {
+            res.push(line);
+        }
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Segment;
+    use style::{StyleAttribute, StyleBuilder};
+
+    #[test]
+    fn test_line() {
+        assert_eq!(Segment::line(None), Segment::new("\n", None, false))
+    }
+
+    #[test]
+    fn test_apply_style() {
+        let bold = StyleBuilder::new()
+            .with_attribute(StyleAttribute::BOLD, true)
+            .build();
+
+        let segments = [
+            Segment::new("foo", None, false),
+            Segment::new("bar", Some(bold.clone()), false),
+        ];
+        let computed: Vec<Segment> = Segment::apply_style(&segments, None).collect();
+        assert_eq!(computed, segments);
+
+        let italic = StyleBuilder::new()
+            .with_attribute(StyleAttribute::ITALIC, true)
+            .build();
+
+        let italic_bold = StyleBuilder::new()
+            .with_attribute(StyleAttribute::ITALIC, true)
+            .with_attribute(StyleAttribute::BOLD, true)
+            .build();
+
+        let expected = [
+            Segment::new("foo", Some(italic.clone()), false),
+            Segment::new("bar", Some(italic_bold), false),
+        ];
+        let computed: Vec<Segment> = Segment::apply_style(&segments, Some(italic)).collect();
+        assert_eq!(computed, expected);
+    }
+
+    #[test]
+    fn test_split_lines() {
+        let lines = [Segment::new("Hello\nWorld", None, false)];
+        let expected = [
+            [Segment::new("Hello", None, false)],
+            [Segment::new("World", None, false)],
+        ];
+        let computed: Vec<Vec<Segment>> = Segment::split_lines(&lines);
+        assert_eq!(computed, expected);
     }
 }
