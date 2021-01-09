d49144a52152a971e2d1710339ea23489a300722
danielsanchezq <daniel.sanchez@iohk.io>
2021-01-09 18:46:00 +0100

    Implemented remaining segment methods

--- segment/src/lib.rs
+++ segment/src/lib.rs
@@ -1,4 +1,5 @@
 use cells::{cell_len, set_cell_size, DEFAULT_CELL_LEN_CACHE};
+use itertools::{EitherOrBoth, Itertools};
 use style::{Style, StyleBuilder};
 
 /// A piece of text with associated style
@@ -88,12 +89,12 @@ impl Segment {
     }
 
     /// Filter segments by ``is_control`` attribute
-    pub fn filter_control<Segments>(
+    pub fn filter_control<'a, Segments>(
         segments: Segments,
         is_control: bool,
-    ) -> impl Iterator<Item = Segment>
+    ) -> impl Iterator<Item = &'a Segment> + 'a
     where
-        Segments: IntoIterator<Item = Segment>,
+        Segments: IntoIterator<Item = &'a Segment> + 'a,
     {
         segments
             .into_iter()
@@ -228,11 +229,132 @@ impl Segment {
         }
         res
     }
+
+    /// Get the length of list of segments
+    pub fn get_line_length(line: &[Segment]) -> usize {
+        line.iter().map(|s: &Segment| s.cell_len()).sum()
+    }
+
+    /// Get the shape (enclosing rectangle) of a list of lines
+    pub fn get_shape(lines: &[&[Segment]]) -> (usize, usize) {
+        let max_width = lines
+            .iter()
+            .map(|s| Segment::get_line_length(s))
+            .max()
+            .unwrap_or(0);
+        (max_width, lines.len())
+    }
+
+    /// Set the shape of a list of lines (enclosing rectangle)
+    pub fn set_shape(
+        lines: &[&[Segment]],
+        width: usize,
+        height: Option<usize>,
+        style: Option<Style>,
+    ) -> Vec<Vec<Segment>> {
+        use itertools::EitherOrBoth::{Both, Left, Right};
+        let height = height.unwrap_or(lines.len());
+        let pad_line = [Segment::new(&" ".repeat(width), style.clone(), false)];
+        lines
+            .iter()
+            .zip_longest((0..height).into_iter())
+            .map(|e| match e {
+                Both(line, _) | Left(line) => {
+                    Segment::adjust_line_length(line, width, style.clone(), None)
+                }
+                Right(_) => pad_line.to_vec(),
+            })
+            .collect()
+    }
+
+    /// Simplify an iterable of segments by combining contiguous segments with the same style
+    pub fn simplify<'a, Segments>(segments: Segments) -> impl Iterator<Item = Segment> + 'a
+    where
+        Segments: IntoIterator<Item = &'a Segment> + 'a,
+    {
+        SimplifiedSegments::new(segments.into_iter())
+    }
+
+    /// Remove all links from an iterable of styles
+    pub fn strip_links<'a, Segments>(segments: Segments) -> impl Iterator<Item = Segment> + 'a
+    where
+        Segments: IntoIterator<Item = &'a Segment> + 'a,
+    {
+        segments.into_iter().map(|segment: &Segment| {
+            if segment.is_control || segment.style.is_none() {
+                segment.clone()
+            } else {
+                Segment::new(
+                    &segment.text,
+                    segment.style.clone().map(|style| style.update_link(None)),
+                    false,
+                )
+            }
+        })
+    }
+
+    /// Remove all styles from an iterable of segments
+    pub fn strip_styles<'a, Segments>(segments: Segments) -> impl Iterator<Item = Segment> + 'a
+    where
+        Segments: IntoIterator<Item = &'a Segment> + 'a,
+    {
+        segments
+            .into_iter()
+            .map(|segment: &Segment| Segment::new(&segment.text, None, segment.is_control))
+    }
+}
+
+struct SimplifiedSegments<'a, Segments: Iterator<Item = &'a Segment>> {
+    inner: Segments,
+    last_segment: Option<Segment>,
+}
+
+impl<'a, Segments: Iterator<Item = &'a Segment>> SimplifiedSegments<'a, Segments> {
+    fn new(segments: Segments) -> Self {
+        Self {
+            inner: segments,
+            last_segment: None,
+        }
+    }
+}
+
+impl<'a, Segments: Iterator<Item = &'a Segment>> Iterator for SimplifiedSegments<'a, Segments> {
+    type Item = Segment;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let last_segment = if self.last_segment.is_none() {
+            self.inner.next().cloned()
+        } else {
+            let last = self.last_segment.clone();
+            self.last_segment = None;
+            last
+        };
+
+        if let Some(last_segment) = last_segment {
+            let mut last_segment = last_segment.clone();
+            while let Some(segment) = self.inner.next() {
+                if last_segment.style == segment.style && !segment.is_control {
+                    last_segment = Segment::new(
+                        &format!("{}{}", last_segment.text, segment.text),
+                        last_segment.style.clone(),
+                        false,
+                    );
+                } else {
+                    self.last_segment = Some(segment.clone());
+                    return Some(last_segment);
+                }
+            }
+            Some(last_segment.clone())
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::Segment;
+    use color::Color;
     use style::{Style, StyleAttribute, StyleBuilder};
 
     #[test]
@@ -331,5 +453,150 @@ mod tests {
             ],
         ];
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_get_line_length() {
+        let lines = [
+            Segment::new("foo", None, false),
+            Segment::new("bar", None, false),
+        ];
+        assert_eq!(Segment::get_line_length(&lines), 6)
+    }
+
+    #[test]
+    fn test_get_shape() {
+        assert_eq!(
+            Segment::get_shape(&[&[Segment::new("Hello", None, false)]]),
+            (5, 1)
+        );
+        assert_eq!(
+            Segment::get_shape(&[
+                &[Segment::new("Hello", None, false)],
+                &[Segment::new("World!", None, false)]
+            ]),
+            (6, 2)
+        );
+    }
+
+    #[test]
+    fn test_set_shape() {
+        let segment = Segment::new("Hello", None, false);
+        let gap_segment = Segment::new("     ", None, false);
+        assert_eq!(
+            Segment::set_shape(&[&[segment.clone()]], 10, None, None),
+            [[segment.clone(), gap_segment.clone()]]
+        );
+
+        assert_eq!(
+            Segment::set_shape(&[&[segment.clone()]], 10, Some(2), None),
+            [
+                vec![segment.clone(), gap_segment.clone()],
+                vec![Segment::new(&" ".repeat(10), None, false)]
+            ]
+        );
+    }
+
+    #[test]
+    fn test_simplify() {
+        let segments = [
+            Segment::new("Hello", None, false),
+            Segment::new(" ", None, false),
+            Segment::new("World!", None, false),
+        ];
+        let expected = [Segment::new("Hello World!", None, false)];
+        assert_eq!(
+            Segment::simplify(&segments).collect::<Vec<Segment>>(),
+            expected
+        );
+
+        let red_style = StyleBuilder::new()
+            .with_color(Color::parse("red").unwrap())
+            .build();
+        let blue_style = StyleBuilder::new()
+            .with_color(Color::parse("blue").unwrap())
+            .build();
+        let segments = [
+            Segment::new("Hello", Some(red_style.clone()), false),
+            Segment::new(" ", Some(red_style.clone()), false),
+            Segment::new("World!", Some(blue_style.clone()), false),
+        ];
+        let expected = [
+            Segment::new("Hello ", Some(red_style.clone()), false),
+            Segment::new("World!", Some(blue_style.clone()), false),
+        ];
+        assert_eq!(
+            Segment::simplify(&segments).collect::<Vec<Segment>>(),
+            expected
+        );
+
+        assert_eq!(Segment::simplify(&[]).collect::<Vec<Segment>>(), []);
+    }
+
+    #[test]
+    fn test_filter_control() {
+        let segments = [
+            Segment::new("foo", None, false),
+            Segment::new("bar", None, true),
+        ];
+
+        assert_eq!(
+            Segment::filter_control(&segments, false)
+                .cloned()
+                .collect::<Vec<Segment>>(),
+            [Segment::new("foo", None, false)]
+        );
+
+        assert_eq!(
+            Segment::filter_control(&segments, true)
+                .cloned()
+                .collect::<Vec<Segment>>(),
+            [Segment::new("bar", None, true)]
+        )
+    }
+
+    #[test]
+    fn test_strip_styles() {
+        let segments = [Segment::new(
+            "foo",
+            Some(
+                StyleBuilder::new()
+                    .with_attribute(StyleAttribute::BOLD, true)
+                    .build(),
+            ),
+            false,
+        )];
+
+        assert_eq!(
+            Segment::strip_styles(&segments).collect::<Vec<Segment>>(),
+            [Segment::new("foo", None, false)]
+        );
+    }
+
+    #[test]
+    fn test_strip_links() {
+        let segments = [Segment::new(
+            "foo",
+            Some(
+                StyleBuilder::new()
+                    .with_attribute(StyleAttribute::BOLD, true)
+                    .with_link("https://www.example.org")
+                    .build(),
+            ),
+            false,
+        )];
+
+        assert_eq!(
+            Segment::strip_links(&segments).collect::<Vec<Segment>>(),
+            [Segment::new(
+                "foo",
+                Some(
+                    StyleBuilder::new()
+                        .with_attribute(StyleAttribute::BOLD, true)
+                        .build()
+                ),
+                false
+            )]
+        );
     }
 }
