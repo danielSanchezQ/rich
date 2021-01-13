ff6cd02e8740e28ea6d64b65dab1657557b76467
danielsanchezq <daniel.sanchez@iohk.io>
2021-01-13 17:33:21 +0100

    Rework console traits

--- console/src/traits.rs
+++ console/src/traits.rs
@@ -1,9 +1,11 @@
 use crate::options::ConsoleOptions;
 use crate::Console;
+use segment::Segment;
 
-// TODO: Use an Iterator to correct type instead of vec of nothing
-// pub type RenderResult = dyn Iterator<Item = dyn Renderable>;
-pub type RenderResult = Vec<()>;
+// TODO: Use an Iterator to correct type instead of vec when RFC 2515 is implemented
+// https://github.com/rust-lang/rust/issues/63063
+//pub type RenderResult = impl Iterator<Item = &Segment>;
+pub type RenderResult = Vec<Segment>;
 
 pub trait Renderable {
     fn rich_console(&self, console: &Console, options: &ConsoleOptions) -> RenderResult;
@@ -20,5 +22,25 @@ trait ConsoleRenderable {
 }
 
 trait RichCast {
-    fn rich(&self) -> dyn ConsoleRenderable;
+    fn rich(&self) -> Segment;
+}
+
+impl RichCast for &str {
+    fn rich(&self) -> Segment {
+        Segment::new(self.to_string().as_str(), None, false)
+    }
+}
+
+impl Renderable for dyn ToString {
+    fn rich_console(&self, console: &Console, options: &ConsoleOptions) -> RenderResult {
+        // get console style
+        // let style = console.style.clone;
+        vec![Segment::new(self.to_string().as_str(), None, false)]
+    }
+}
+
+impl Renderable for &str {
+    fn rich_console(&self, console: &Console, options: &ConsoleOptions) -> RenderResult {
+        vec![Segment::new(self.to_string().as_str(), None, false)]
+    }
 }
